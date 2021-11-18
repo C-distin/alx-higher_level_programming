f77db40eabdebfb406fba69cdf1abe12739b5aaf
C-distin <paannang@st.ug.edu.gh>
2021-11-18 15:38:05 +0000

    Add 5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    # Import variable 'a' from file 'variable_load_5.py'
+    from variable_load_5 import a
+
+    # print the value of variable 'a'
+    print(a)
