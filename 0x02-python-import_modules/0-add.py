e1eb886a79e2cca6b928f707d3f2095e3a35384a
C-distin <paannang@st.ug.edu.gh>
2021-11-18 10:26:37 +0000

    Add 1-calculation.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+# Import the add() function from the file add_0.py
 if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
 
+    # Call the add() function from the add_0.py file
     print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# Import the calculator module from the calculator_1.py file
+if __name__ == "__main__":
+    import calculator_1 as calc
+
+    a = 10
+    b = 5
+
+    # Call the add() function from the calculator module
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+
+    # Call the sub() function from the calculator module
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+
+    # Call the mul() function from the calculator module
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+
+    # Call the div() function from the calculator module
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
