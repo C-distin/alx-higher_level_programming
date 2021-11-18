90796b297d214f122389850809c6f07d4269c559
C-distin <paannang@st.ug.edu.gh>
2021-11-18 12:00:49 +0000

    Add 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    # Import modules
+    import sys
+
+    # prints the results of the addition of the arguments
+    # print(sum(int(arg) for arg in sys.argv[1:]))
+    sum = 0
+
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+
+    print(sum)
