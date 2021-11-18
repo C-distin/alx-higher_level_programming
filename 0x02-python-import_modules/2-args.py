5a216caaaafe1d40c4df4033e727f7e261cb0fcc
C-distin <paannang@st.ug.edu.gh>
2021-11-18 15:58:02 +0000

    update 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     args = len(sys.argv) - 1
 
     if args == 0:
-        print("{} arguments".format(args))
+        print("{:d} arguments".format(args))
 
     elif args == 1:
-        print("{} argument:".format(args))
+        print("{:d} argument:".format(args))
 
     else:
-        print("{} arguments:".format(args))
+        print("{:d} arguments:".format(args))
 
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{:d}: {:d}".format(i, sys.argv[i]))
