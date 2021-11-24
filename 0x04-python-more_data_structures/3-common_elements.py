487a08818c9e2c02fe4f079697fdcf5528a045fa
C-distin <paannang@st.ug.edu.gh>
2021-11-24 10:00:55 +0000

    Add 4-only_diff_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    c_set = set()
-    for i in zip(set_1, set_2):
-        if i[0] == i[1]:
-            c_set.add(i[0])
-    return c_set
+    return set_1 & set_2
