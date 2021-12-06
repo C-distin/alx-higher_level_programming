a60c1659b200e148951d4bc051ab346c1c51f4b1
C-distin <paannang@st.ug.edu.gh>
2021-12-06 14:15:29 +0000

    Add safe print division

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    j = 0
-    for i in range(x):
+    i = 0
+    for index in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            j += 1
+            print("{:d}".format(my_list[index]), end="")
+            i += 1
         except (ValueError, TypeError):
             pass
     print()
-    return j
+    return i
