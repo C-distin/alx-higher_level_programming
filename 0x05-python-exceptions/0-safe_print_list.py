eb226909384ea316a0138471a5faff32e8a39122
C-distin <paannang@st.ug.edu.gh>
2021-12-06 12:57:18 +0000

    Add safe print integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            while i < x:
+                print("{:d}".format(my_list[i]), end="")
+                i += 1
         except IndexError:
-            break
-    print()
-    return i + 1
+            pass
+        print()
+        return i
