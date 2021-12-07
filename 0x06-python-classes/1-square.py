d2bd0da22f84ba5df2bae351100247714c613832
C-distin <paannang@st.ug.edu.gh>
2021-12-07 10:04:26 +0000

    Add square init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the size of the square square
+        """
+        self.__size = size
