7b3c89bf707c8b500d23a54b0297d3bc2ced5990
C-distin <paannang@st.ug.edu.gh>
2021-12-07 10:51:47 +0000

    Add square getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that defines a square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square
+
+        Args:
+            size: size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter for size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for size
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return self.__size ** 2
