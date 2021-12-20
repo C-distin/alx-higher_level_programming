a92bc090508d63992d07af2d91e09277fedeb8c9
C-distin <paannang@st.ug.edu.gh>
2021-12-20 17:10:29 +0000

    add inherit from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains a function that determines if one class is inherited
+directly or indirectly from another class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True if the object is an instance of the specified class
+    or inherited from it
+
+    Args:
+        obj: the object to be tested
+        a_class: the class to be tested against
+
+    Returns:
+        True if the object is an instance of the specified class
+        or inherited from it
+        False if otherwise
+    """
+    return isinstance(obj, a_class) or type(obj) == a_class
