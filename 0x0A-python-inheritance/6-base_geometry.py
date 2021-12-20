98b7d53111d810ffc6832815ba063e3b714dd9b5
C-distin <paannang@st.ug.edu.gh>
2021-12-20 18:07:22 +0000

    add base geometry II

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Define a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Class that defines a BaseGeometry
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an Exception
+        """
+        raise Exception("area() is not implemented")
