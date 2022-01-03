57c9aa3b8be02a40664ac0fa6e297b2eec1401cb
C-distin <paannang@st.ug.edu.gh>
2022-01-03 13:22:21 +0000

    add base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,3 +47,12 @@ class Base:
                 f.write(cls.to_json_string(list(map(lambda x:
                                                     x.to_dictionary(),
                                                     list_objs))))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
