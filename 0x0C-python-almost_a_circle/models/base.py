fa093d50a494784f6a8bb5f231a39b291c9651e7
C-distin <paannang@st.ug.edu.gh>
2022-01-03 12:36:11 +0000

    add base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        converts a list of dictionaries to a json string
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
