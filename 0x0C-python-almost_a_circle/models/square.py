9597fb8ac8c13dd12c4c6d3de665ebb68dc12d22
C-distin <paannang@st.ug.edu.gh>
2022-01-03 12:01:33 +0000

    squre 3

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,9 +36,9 @@ class Square(Rectangle):
         setter for size
         """
         if type(value) != int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("size must be > 0")
+            raise ValueError("width must be > 0")
 
         self.width = value
         self.height = value
