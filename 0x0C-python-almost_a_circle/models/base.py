271ebbe814a638a680e171bb2f40e820c29cf3ce
C-distin <paannang@st.ug.edu.gh>
2022-01-03 13:01:31 +0000

    fix errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,9 +24,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
-        converts a list of dictionaries to a json string
+        returns the JSON string representation of list_dictionaries
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
