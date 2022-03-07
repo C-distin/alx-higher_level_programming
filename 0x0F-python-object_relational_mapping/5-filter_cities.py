67e890d41539817a24dad2984e3d75763f9e5a84
C-distin <paannang@st.ug.edu.gh>
2022-03-07 10:44:56 +0000

    safe cities filter

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains a function that safe filters cities by user input.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+        FROM cities JOIN states ON cities.state_id = states.id\
+        WHERE states.name LIKE %s ORDER BY cities.id ASC", (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(", ".join(map(str, row)))
+    cursor.close()
+    db.close()
