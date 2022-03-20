a1f466d1172682a170cdae4ad65f32b15f72d8f2
C-distin <paannang@st.ug.edu.gh>
2022-03-20 12:36:58 +0000

    fix errors

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to take in a URL, send a request to that URL, and display the size of the body of the response
-curl -sI "$1" | grep Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
